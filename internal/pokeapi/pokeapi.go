96dbe38ece530d98ad8be32ef0ae1c56d77d613f
dsledge1 <david.sledge1@gmail.com>
2026-05-26 21:59:40 -0400

    explore function implemented

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -20,6 +20,52 @@ type APIResponse struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
 	} `json:"results"`
+	Pokemon_encounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"pokemon"`
+	} `json:"pokemon_encounters"`
+}
+
+func GetPokemon(url string, cache *pokecache.Cache) (APIResponse, error) {
+	fmt.Println("Scanning area for Pokemon")
+	cachedData, ok := cache.Get(url)
+	if ok {
+		var cachedResponse APIResponse
+		err := json.Unmarshal(cachedData, &cachedResponse)
+		if err != nil {
+			return APIResponse{}, err
+		}
+		// TODO Print the names of the Pokemon found in the cached response
+		for _, poke := range cachedResponse.Pokemon_encounters {
+			fmt.Println("- " + poke.Pokemon.Name)
+		}
+		return cachedResponse, nil
+	}
+	res, err := http.Get(url)
+	if err != nil {
+		return APIResponse{}, err
+	}
+	defer res.Body.Close()
+	var pokemon APIResponse
+	bod := json.NewDecoder(res.Body)
+
+	err = bod.Decode(&pokemon)
+	if err != nil {
+		return APIResponse{}, err
+	}
+
+	fmt.Println("Caching data...")
+	cachingPokemon, err := json.Marshal(pokemon)
+	if err != nil {
+		return APIResponse{}, err
+	}
+	cache.Add(url, cachingPokemon) //Get Pokemon struct right?
+	for _, poke := range pokemon.Pokemon_encounters {
+		fmt.Println("- " + poke.Pokemon.Name)
+	}
+	return pokemon, nil
 }
 
 func GetLocations(url string, cache *pokecache.Cache) (APIResponse, error) {
--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func main() {
 		Limit:    20,
 		Offset:   0,
 		Next:     pokeapi.APIEndpoint + "location-area?offset=0&limit=20",
+		Current:  pokeapi.APIEndpoint + "location-area?offset=0&limit=20",
 		Previous: pokeapi.APIEndpoint + "location-area?offset=0&limit=20",
 		Cache:    pokecache.NewCache(5 * time.Minute),
 	}
@@ -42,15 +43,23 @@ func main() {
 			description: "Displays name of previous 20 locations",
 			callback:    commandMapb,
 		},
+		"explore": {
+			name:        "explore",
+			description: "Displays a list of all Pokemon located at a particular location",
+			callback:    exploreLocation,
+		},
 	}
 
 	for {
 		fmt.Print("Pokedex >")
 		scanner.Scan()
 		input := cleanInput(scanner.Text())
+		if len(input) == 0 {
+			continue
+		}
 		command, ok := supportedCommands[input[0]]
 		if ok {
-			err := command.callback(config)
+			err := command.callback(input, config)
 			if err != nil {
 				fmt.Printf("Error executing command: %v\n", err)
 			}
@@ -63,45 +72,63 @@ func main() {
 
 }
 
-func commandExit(config *config) error {
+func commandExit(args []string, config *config) error {
 	fmt.Print("Closing the Pokedex... Goodbye!\n")
 	os.Exit(0)
 	return nil
 }
 
-func help(config *config) error {
+func help(args []string, config *config) error {
 	fmt.Println("Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex")
 	return nil
 }
 
-func commandMap(config *config) error {
+func commandMap(args []string, config *config) error {
 	loc, err := pokeapi.GetLocations(config.Next, config.Cache)
 	if err != nil {
 		return err
 	}
+
+	config.Current = config.Next
 	config.Next = loc.Next
 	config.Previous = loc.Previous
 	return nil
 }
 
-func commandMapb(config *config) error {
+func commandMapb(args []string, config *config) error {
 	loc, err := pokeapi.GetLocations(config.Previous, config.Cache)
 	if err != nil {
 		return err
 	}
+	config.Current = config.Previous
 	config.Next = loc.Next
 	config.Previous = loc.Previous
 	return nil
+}
+
+func exploreLocation(args []string, config *config) error {
+	if len(args) < 2 {
+		return fmt.Errorf("usage: explore <location-area-name>")
+	}
+
+	location := args[1]
+	_, err := pokeapi.GetPokemon(pokeapi.APIEndpoint+"location-area/"+location, config.Cache)
+	if err != nil {
+		return err
+	}
+	return nil
+
 }
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func(*config) error
+	callback    func([]string, *config) error
 }
 
 type config struct {
 	Next     string `json: "next"`
+	Current  string `json: current`
 	Previous string `json: "previous"`
 	Limit    int    `json: "limit"`
 	Offset   int    `json: "offset"`
